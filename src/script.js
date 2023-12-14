fc2c2eecf7e58357e2accdf6a039281ac97da1ad
Mark A. <mark001616@gmail.com>
2023-12-14 09:29:13 -0500

    Return API result to terminal

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
 import "./style.css";
 import displayAllCities from "./Displayer.js";
+import { getCityWeather } from "./Api.js";
 
 displayAllCities();
+let searchedCity = "";
+
+const searchCityInput = document.querySelector(".search-city-input");
+
+searchCityInput.addEventListener("change", (e) => {
+  e.preventDefault();
+  searchedCity = e.target.value;
+  console.log(searchedCity);
+  let test = getCityWeather(searchedCity);
+  console.log(test);
+});
