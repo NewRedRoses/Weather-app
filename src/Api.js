a0790d1adfcbcec9ec34b5b9c5e42fc71a61b637
Mark A. <mark001616@gmail.com>
2023-12-13 21:03:32 -0500

    Add function to return weather object given city name

---
+++ src/Api.js
@@ -0,0 +1,10 @@
+async function getCityWeather(cityName) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=92a70ec39e5e421380c180722230412&q=${cityName}&aqi=no`,
+    { mode: "cors" },
+  );
+  const cityWeatherData = await response.json();
+  return cityWeatherData;
+}
+
+export { getCityWeather };
