51f304d13a2eff1ae259e973a366c6880e16598f
Mark A. <mark001616@gmail.com>
2023-12-14 22:56:00 -0500

    Add function to display data based on input

--- src/Displayer.js
+++ src/Displayer.js
@@ -1,5 +1,6 @@
 const listOfCities = ["Newark", "Trenton", "Jersey City", "Hoboken", "NYC"];
-export default function displayAllCities() {
+
+function displayAllCities() {
   const citiesContainer = document.querySelector(".list-of-cities");
   listOfCities.forEach((city) => {
     const cityContainter = document.createElement("li");
@@ -7,3 +8,17 @@ export default function displayAllCities() {
     citiesContainer.appendChild(cityContainter);
   });
 }
+async function displayWeatherDataToPanel(weatherResponse) {
+  let weatherData = await weatherResponse;
+
+  const cityNameContainer = document.querySelector(".city-name");
+  cityNameContainer.textContent = weatherData.location.name;
+
+  const cityRegionContainer = document.querySelector(".city-region");
+  cityRegionContainer.textContent = weatherData.location.region;
+
+  const cityCountryContainer = document.querySelector(".city-country");
+  cityCountryContainer.textContent = weatherData.location.country;
+}
+
+export { displayAllCities, displayWeatherDataToPanel };
