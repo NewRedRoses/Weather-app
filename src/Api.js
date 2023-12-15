d7f52f10e5306e38c630c6912491352966c1a194
Mark A. <mark001616@gmail.com>
2023-12-15 18:47:28 -0500

    Use https instead of http

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 async function getCityWeatherPromise(cityName) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=92a70ec39e5e421380c180722230412&q=${cityName}&aqi=no`,
+      `https://api.weatherapi.com/v1/current.json?key=92a70ec39e5e421380c180722230412&q=${cityName}&aqi=no`,
       { mode: "cors" },
     );
     const cityWeatherData = await response.json();
