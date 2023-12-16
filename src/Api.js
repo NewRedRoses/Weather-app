267d3aae6b05f59d4d5fbeab74178ec4aed26f82
Mark A. <mark001616@gmail.com>
2023-12-16 16:50:58 -0500

    Add forecast api function & refactor key

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,8 @@
+const apiKey = "92a70ec39e5e421380c180722230412";
 async function getCityWeatherPromise(cityName) {
   try {
     const response = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=92a70ec39e5e421380c180722230412&q=${cityName}&aqi=no`,
+      `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`,
       { mode: "cors" },
     );
     const cityWeatherData = await response.json();
@@ -10,4 +11,16 @@ async function getCityWeatherPromise(cityName) {
     console.log(error);
   }
 }
-export { getCityWeatherPromise };
+async function getCityWeatherForecastPromise(cityName) {
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&=${cityName}`,
+      { mode: "cors" },
+    );
+    const cityForecastData = await response.json();
+    return cityForecastData;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export { getCityWeatherPromise, getCityWeatherForecastPromise };
