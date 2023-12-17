f9a5eeacc96e0e6c13b4ec43cf38c24c0874fb84
Mark A. <mark001616@gmail.com>
2023-12-17 06:34:07 -0500

    Delete previous city data

--- src/script.js
+++ src/script.js
@@ -6,10 +6,15 @@ import { getCityWeatherForecastPromise } from "./Api.js";
 let searchedCity = "";
 
 const searchCityInput = document.querySelector(".search-city-input");
+const hourlyForecastDataContainer = document.querySelector(
+  ".city-hourly-forecast-container",
+);
 
 searchCityInput.addEventListener("change", (e) => {
   e.preventDefault();
   searchedCity = e.target.value;
+  // Works ok for now, don't like the second delay after typing city name.
+  hourlyForecastDataContainer.innerHTML = "";
   // console.log(searchedCity);
   let weatherObject = getCityWeatherForecastPromise(searchedCity).catch(
     (error) => {
